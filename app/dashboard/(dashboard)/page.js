d9a72ce0f8c881298ec2cdcf3d0db8c7191816ae
Tijani oluwadamilola Abdul Azeez <111458165+DAMMYTJ@users.noreply.github.com>
2024-07-28 10:47:34 +0100

    Add files via upload

---
+++ app/dashboard/(dashboard)/page.js
@@ -0,0 +1,15 @@
+import DashboardDefault from "@/components/UI/Dashboard/Main/DashboardDefault";
+
+export const metadata = {
+  title: "(Dashboard) - Benjamin Nkem",
+};
+
+const DefaultUserDashboard = () => {
+  return (
+    <>
+      <DashboardDefault />
+    </>
+  );
+};
+
+export default DefaultUserDashboard;
